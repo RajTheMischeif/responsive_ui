a2e67fe3912ce54472d620aee8099497b9c5376b
Mohan <rajm16030@gmail.com>
2024-10-08 08:31:46 +0530

    final change

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:responsive_ui/Model/rental_model.dart';
@@ -6,38 +7,55 @@ import 'Model/CountryModel.dart';
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
   final List<Country> ctnLt = [
-    Country(country: 'India', url: 'assets/image/taj.jpg'),
-    Country(country: 'USA', url: 'assets/image/usa.jpg'),
-    Country(country: 'UK', url: 'assets/image/uk.jpg'),
-    Country(country: 'China', url: 'assets/image/china.jpg'),
-    Country(country: 'Australia', url: 'assets/image/aus.jpeg'),
+    Country(
+        country: 'India',
+        url:
+            'https://img.freepik.com/free-photo/beautiful-vertical-shot-taj-mahal-building-agra-india-with-trees-nearby_181624-34372.jpg?t=st=1728317073~exp=1728320673~hmac=245a6905cd6c31595441b1707b88daf0a5e7129b2dfb9f91e1d5b9cdda50a28b&w=360'),
+    Country(country: 'USA', url: 'https://wallpapercave.com/wp/rdbGBa5.jpg'),
+    Country(
+        country: 'UK',
+        url:
+            'https://th.bing.com/th/id/OIP.04jT6P0mdDoFBx439xUYhwAAAA?rs=1&pid=ImgDetMain'),
+    Country(
+        country: 'China',
+        url:
+            'https://th.bing.com/th/id/OIP.LAXdoqc9eBP3Y7HfVzFMIQHaLL?rs=1&pid=ImgDetMain'),
+    Country(
+        country: 'Australia',
+        url:
+            'https://th.bing.com/th/id/OIP.qrCgtTqE3GylvqpoatIydQHaJ4?rs=1&pid=ImgDetMain'),
   ];
   final List<RentalModel> rntLt = [
     RentalModel(
         price: r'$400/Night',
         details: 'Carinthia lake see breakfast lorem',
         roomDetails: 'private room/4 beds',
-        url: 'assets/image/house/house1.jpg'),
+        url:
+            'https://greenhaveninc.com/wp-content/uploads/2020/08/house-with-front-yard-with-green-grass-and-landscaping--1080x675.jpg'),
     RentalModel(
         price: r'$100/Night',
         details: 'Carinthia lake see breakfast lorem',
         roomDetails: 'private room/2 beds',
-        url: 'assets/image/house/house2.jpg'),
+        url:
+            'https://www.forbes.com/advisor/wp-content/uploads/2021/06/featured-image-front-yard-landscaping.jpeg'),
     RentalModel(
         price: r'$300/Night',
         details: 'Carinthia lake see breakfast lorem',
         roomDetails: 'private room/3 beds',
-        url: 'assets/image/house/house3.jpg'),
+        url:
+            'https://www.homestratosphere.com/wp-content/uploads/2016/04/48front-yard-garden.jpg'),
     RentalModel(
         price: r'$800/Night',
         details: 'Carinthia lake see breakfast lorem',
         roomDetails: 'private room/7 beds',
-        url: 'assets/image/house/house4.jpg'),
+        url:
+            'https://probiltgroup.com/wp-content/uploads/2022/11/add-a-level-morris-county-nj-probilt-group-llc-header-9-1024x683.jpg'),
     RentalModel(
         price: r'$250/Night',
         details: 'Carinthia lake see breakfast lorem',
         roomDetails: 'private room/5 beds',
-        url: 'assets/image/house/house5.jpg'),
+        url:
+            'https://www.capstonefundinggroup.com/wp-content/uploads/2017/03/bigstock-Luxury-house-with-beautiful-la-91049936-1024x683.jpg'),
   ];
   @override
   Widget build(BuildContext context) {
@@ -49,7 +67,8 @@ class MyHomePage extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                color: const Color.fromRGBO(246, 240, 255, 1),
+                color: const Color.fromARGB(255, 225, 225, 225),
+                // const Color.fromRGBO(246, 240, 255, 1),
                 height: Get.height * 0.25,
                 child: Padding(
                   padding: EdgeInsets.all(Get.height * 0.015),
@@ -157,7 +176,8 @@ class MyHomePage extends StatelessWidget {
                               rntLt[index].url,
                               rntLt[index].price,
                               rntLt[index].details,
-                              rntLt[index].roomDetails),
+                              rntLt[index].roomDetails,
+                              false),
                         ),
                       ),
                     ),
@@ -168,22 +188,34 @@ class MyHomePage extends StatelessWidget {
                 height: Get.height * 0.035,
               ),
               Padding(
-                padding: EdgeInsets.all(Get.height * 0.015),
+                padding: EdgeInsets.only(
+                    left: Get.height * 0.015, right: Get.height * 0.015),
                 child: Stack(
                   children: [
                     Container(
                       decoration: BoxDecoration(
-                        color: Colors.red,
+                        color: const Color.fromARGB(255, 225, 225, 225),
                         borderRadius: BorderRadius.circular(Get.height * 0.02),
                       ),
                       height: Get.height * 0.35,
                       child: ClipRRect(
-                          borderRadius:
-                              BorderRadius.circular(Get.height * 0.02),
-                          child: Image.asset(
-                            'assets/image/hotel_pic.jpg',
-                            fit: BoxFit.fill,
-                          )),
+                        borderRadius: BorderRadius.circular(Get.height * 0.02),
+                        child: CachedNetworkImage(
+                          progressIndicatorBuilder:
+                              (context, url, loadingProgress) {
+                            return Center(
+                              child: CircularProgressIndicator(
+                                strokeWidth: 3,
+                                color: Colors.black,
+                                value: loadingProgress.progress,
+                              ),
+                            );
+                          },
+                          imageUrl:
+                              'https://thumbs.dreamstime.com/b/two-girls-drinking-tea-18110920.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                     Padding(
                       padding: EdgeInsets.all(Get.height * 0.025),
@@ -218,30 +250,27 @@ class MyHomePage extends StatelessWidget {
               ),
               Padding(
                 padding: EdgeInsets.all(Get.height * 0.015),
-                child: Column(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Most viewed',
-                          style: TextStyle(fontSize: Get.height * 0.03),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: Get.height * 0.035,
-                    ),
-                    Column(
-                      children: List.generate(
-                          3,
-                          (index) => container2(
-                              rntLt[index].url,
-                              rntLt[index].price,
-                              rntLt[index].details,
-                              rntLt[index].roomDetails)),
+                    Text(
+                      'Most viewed',
+                      style: TextStyle(fontSize: Get.height * 0.03),
                     ),
                   ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(Get.height * 0.015),
+                child: Column(
+                  children: List.generate(
+                      3,
+                      (index) => container2(
+                          rntLt[index].url,
+                          rntLt[index].price,
+                          rntLt[index].details,
+                          rntLt[index].roomDetails,
+                          true)),
                 ),
               ),
             ],
@@ -261,13 +290,23 @@ class MyHomePage extends StatelessWidget {
             height: Get.height * 0.213,
             width: Get.width * 0.4,
             decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 225, 225, 225),
               borderRadius: BorderRadius.circular(Get.height * 0.02),
             ),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(Get.height * 0.02),
-              child: Image.asset(
-                url,
-                fit: BoxFit.fill,
+              child: CachedNetworkImage(
+                progressIndicatorBuilder: (context, url, loadingProgress) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      strokeWidth: 3,
+                      color: Colors.black,
+                      value: loadingProgress.progress,
+                    ),
+                  );
+                },
+                imageUrl: url,
+                fit: BoxFit.cover,
               ),
             ),
           ),
@@ -283,10 +322,13 @@ class MyHomePage extends StatelessWidget {
     );
   }
 
-  Widget container2(String url, String price, String details, String roomDtl) {
+  Widget container2(
+      String url, String price, String details, String roomDtl, bool check) {
     return Padding(
-      padding:
-          EdgeInsets.only(right: Get.width * 0.03, bottom: Get.height * 0.02),
+      padding: EdgeInsets.only(
+        right: check ? 0.0 : Get.width * 0.03,
+        bottom: check ? Get.height * 0.02 : 0.0,
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -294,17 +336,31 @@ class MyHomePage extends StatelessWidget {
             alignment: Alignment.topRight,
             children: [
               Container(
-                height: Get.height * 0.213,
-                width: Get.width * 0.65,
+                height: Get.height * 0.22,
+                width: check ? Get.width : null,
                 decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 225, 225, 225),
                   borderRadius: BorderRadius.circular(Get.height * 0.02),
                 ),
                 child: ClipRRect(
-                    borderRadius: BorderRadius.circular(Get.height * 0.02),
-                    child: Image.asset(
-                      url,
-                      fit: BoxFit.fill,
-                    )),
+                  borderRadius: BorderRadius.circular(Get.height * 0.02),
+                  child: CachedNetworkImage(
+                    imageUrl: url,
+                    progressIndicatorBuilder: (context, url, loadingProgress) {
+                      return SizedBox(
+                        width: check ? null : Get.width / 1.4,
+                        height: check ? null : Get.height * 0.22,
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.black,
+                            value: loadingProgress.progress,
+                          ),
+                        ),
+                      );
+                    },
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
               Padding(
                 padding: EdgeInsets.all(Get.height * 0.01),
@@ -339,6 +395,11 @@ class MyHomePage extends StatelessWidget {
             roomDtl,
             style: TextStyle(fontSize: Get.height * 0.015, color: Colors.grey),
           ),
+          check
+              ? SizedBox(
+                  height: Get.height * 0.035,
+                )
+              : const SizedBox(),
         ],
       ),
     );
